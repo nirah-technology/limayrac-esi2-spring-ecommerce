a5dcf33dd19e5cb56681c2f205286c4eb2612c21
Nicolas Metivier <nicolas.a.metivier@gmail.com>
2026-04-03 11:21:56 +0200

    Init project

---
+++ src/main/java/fr/nirahtech/limayrac/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package fr.nirahtech.limayrac.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
