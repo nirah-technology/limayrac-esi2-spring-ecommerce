3615fc024dd60e276b90b1477282e1e3f7bb60e5
Nicolas Metivier <nicolas.a.metivier@gmail.com>
2026-04-08 13:58:31 +0200

    Clear wotkspace

--- src/main/java/fr/nirahtech/limayrac/ecommerce/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.nirahtech.limayrac.ecommerce;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductsRepository extends JpaRepository<Product, Long> {
-    
-}
